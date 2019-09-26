efb83b60ed7ef8fc70717a44eeae1a4664051de4
alanrslima <alanronison.lima@gmail.com>
2019-09-26 16:56:32 -0300

    Adding serialiazers in restaurant service #72
    Co-authored-by: Sara Silva <saracsas2@gmail.com>

---
+++ restaurant_service/api/serializers.py
@@ -0,0 +1,57 @@
+from rest_framework.serializers import ModelSerializer
+import restaurant_service.models as model
+
+class ShoppingSerializer(ModelSerializer):
+  
+  class Meta:
+    model = model.Shopping
+    fields = '__all__'
+
+
+class OpeningHoursSerializer(ModelSerializer):
+  
+  class Meta:
+    model = model.OpeningHours
+    fields = '__all__'
+
+
+class RestaurantCategorySerializer(ModelSerializer):
+  
+  class Meta:
+    model = model.RestaurantCategory
+    fields = '__all__'
+
+
+class RestaurantSerializer(ModelSerializer):
+  
+  class Meta:
+    model = model.Restaurant
+    fields = '__all__'
+
+
+class MenuSerializer(ModelSerializer):
+  
+  class Meta:
+    model = model.Menu
+    fields = '__all__'
+
+
+class ItemMenuSerializer(ModelSerializer):
+  
+  class Meta:
+    model = model.ItemMenu
+    fields = '__all__'
+
+
+class ItemCategorySerializer(ModelSerializer):
+  
+  class Meta:
+    model = model.ItemCategory
+    fields = '__all__'
+
+
+class CategoryOptionSerializer(ModelSerializer):
+  
+  class Meta:
+    model = model.CategoryOption
+    fields = '__all__'
---
+++ restaurant_service/api/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import renderegistrationUser
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import authentication, permissions
+from django.contrib.auth.models import User
