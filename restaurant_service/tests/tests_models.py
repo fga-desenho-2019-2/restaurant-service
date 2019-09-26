ccc53b8e85f3cbbacd3fe65ab3f22a27646ed910
alanrslima <alanronison.lima@gmail.com>
2019-09-26 19:43:19 -0300

    Finishing models tests #72
    Co-authored-by: Sara Silva <saracsas2@gmail.com>

--- restaurant_service/tests/tests_models.py
+++ restaurant_service/tests/tests_models.py
@@ -39,7 +39,7 @@ class TestOpeningHours(TestCase):
 
 class TestRestaurantCategory(TestCase):
   def setUp(self):
-    self.restaurant_category = mommy.make(RestaurantCategory, name = 'Fast-food' )
+    self.restaurant_category = mommy.make(RestaurantCategory, name = 'Fast Food' )
       
   def test_shopping_creation(self):
     self.assertTrue(isinstance(self.restaurant_category, RestaurantCategory))
@@ -84,5 +84,80 @@ class TestRestaurant(TestCase):
     )
       
   def test_shopping_creation(self):
-      self.assertTrue(isinstance(self.restaurant, Restaurant))
-      self.assertEquals(self.restaurant.__str__(), self.restaurant.name)
+    self.assertTrue(isinstance(self.restaurant, Restaurant))
+    self.assertEquals(self.restaurant.__str__(), self.restaurant.name)
+
+
+class TestMenu(TestCase):
+  def setUp(self):
+    self.restaurant = mommy.make(Restaurant, 
+      cnpj = '13339532000109',
+      name = 'Burger King',
+      number = 112,
+      description = 'O melhor FastFood',
+      phone = '99999999999',
+    )
+    self.menu = mommy.make(Menu, description = 'Combos', restaurant = self.restaurant)
+      
+  def test_shopping_creation(self):
+    self.assertTrue(isinstance(self.menu, Menu))
+    self.assertEquals(self.menu.__str__(), self.menu.description)
+
+
+class TestItemMenu(TestCase):
+  def setUp(self):
+    self.menu = mommy.make(Menu, description = 'Combos')
+    self.item_menu = mommy.make(ItemMenu, 
+      name = 'X-Salada',
+      value = 20.50,
+      description = 'Sanduiche muito gostoso', 
+      preparation_time = '00:15:00',
+      menu = self.menu
+    )
+      
+  def test_shopping_creation(self):
+    self.assertTrue(isinstance(self.item_menu, ItemMenu))
+    self.assertEquals(self.item_menu.__str__(), self.item_menu.name)
+
+
+class TestItemCategory(TestCase):
+  def setUp(self):
+    self.item_menu = mommy.make(ItemMenu, 
+      name = 'X-Salada',
+      value = 20.50,
+      description = 'Sanduiche muito gostoso', 
+      preparation_time = '00:15:00',
+    )
+    self.item_category = mommy.make(ItemCategory, 
+      title = 'Escolha um molho',
+      description = 'Escolha apenas 1 molho',
+      required = True, 
+      number_of_items = 1,
+      item_menu = self.item_menu
+    )
+      
+  def test_shopping_creation(self):
+    self.assertTrue(isinstance(self.item_category, ItemCategory))
+    self.assertEquals(self.item_category.__str__(), self.item_category.title)
+
+
+class TestCategoryOption(TestCase):
+  def setUp(self):
+    self.item_category = mommy.make(ItemCategory, 
+      title = 'Escolha um molho',
+      description = 'Escolha apenas 1 molho',
+      required = True, 
+      number_of_items = 1,
+    )
+    self.category_option = mommy.make(CategoryOption, 
+      title = 'Maionese',
+      description = 'Maionese especial do BK',
+      value = 1.00, 
+      item_category = self.item_category,
+    )
+      
+  def test_shopping_creation(self):
+    self.assertTrue(isinstance(self.category_option, CategoryOption))
+    self.assertEquals(self.category_option.__str__(), self.category_option.title)
+
+
