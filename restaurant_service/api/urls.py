5a4e88ead03a0576743a7a1a3d45704bc35592d1
Sara Silva <saracsas2@gmail.com>
2019-09-29 13:16:55 -0300

    Add method for requests by restaurant pk

--- restaurant_service/api/urls.py
+++ restaurant_service/api/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
-from .views import post_or_get
+from .views import (
+    post_or_get_restaurant,
+    restaurant_by_pk,
+)
 
 urlpatterns = [
-    path('restaurant/', post_or_get, name="restaurant"),
+    path('restaurant/', post_or_get_restaurant, name="restaurant"),
+    path('restaurant/<int:pk>/', restaurant_by_pk, name="restaurant_by_pk"),
 ]
