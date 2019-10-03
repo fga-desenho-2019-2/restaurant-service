eb8fb0cd9ab2ecf929737ba7f62205b3f7f412d4
MatheusBlanco <msallesblanco@gmail.com>
2019-10-03 19:18:51 -0300

    Adding qrcode generator method, by shopping cnpj
    Co-authored-by: Pedro Feo <pheofo@gmail.com>

--- restaurant_service/api/models.py
+++ restaurant_service/api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Shopping(models.Model):
   created = models.DateTimeField(auto_now_add=True)
-  cnpj = models.CharField(primary_key=True, max_length=16)
+  cnpj = models.IntegerField(primary_key=True)
   name = models.CharField(max_length=100)
   city = models.CharField(max_length=100)
   state = models.CharField(max_length=30)
@@ -11,6 +11,7 @@ class Shopping(models.Model):
   cep = models.CharField(max_length=8)
   number = models.IntegerField()
   phone = models.CharField(max_length=12)
+#  identification = models.IntegerField(primary_key=True)
   
   class Meta:
     ordering = ['created']
@@ -19,7 +20,6 @@ class Shopping(models.Model):
 
   def __str__(self):
     return self.name
-
 
 class OpeningHours(models.Model):
   day = models.CharField(max_length=50)
