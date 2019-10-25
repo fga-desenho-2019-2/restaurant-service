fde5af6ecfe1634b86a2528dc3e369e42e33b8d2
Sara Silva <saracsas2@gmail.com>
2019-10-25 01:19:47 -0300

    Changing views for template use

--- restaurant_service/api/views.py
+++ restaurant_service/api/views.py
@@ -102,188 +102,62 @@ class RestaurantView(TemplateClass):
         elif request.method == 'DELETE':
             return TemplateClass.delete(Restaurant, RestaurantSerializer, pk)
 
-#@api_view(['POST', 'GET'])
-#def post_or_get_restaurant(request):
-#    """
-#    List all restaurant, or create a new restaurant.
-#    """
-#
-#    if request.method == 'GET':
-#        restaurants = Restaurant.objects.all()
-#        serializer = RestaurantSerializer(restaurants, many=True)
-#        return JsonResponse(serializer.data, safe=False)
-#
-#    elif request.method == 'POST':
-#        data = JSONParser().parse(request)
-#        serializer = RestaurantSerializer(data=data)
-#        if serializer.is_valid():
-#            serializer.save()
-#            return JsonResponse(serializer.data, status=201, safe=False)
-#        return JsonResponse(serializer.errors, status=400, safe=False)
+class ShoppingView(TemplateClass):
+    @api_view(['GET', 'POST'])
+    def handle_get_or_post(request):
+        if request.method == 'GET':
+            return TemplateClass.get_all(Shopping, ShoppingSerializer)
+        if request.method == 'POST':
+            return TemplateClass.post(request, ShoppingSerializer)
 
+    @api_view(['GET', 'PUT', 'DELETE'])
+    def handle_get_by_pk(request, pk):
+        if request.method == 'GET':
+            return TemplateClass.get_by_pk(Shopping, ShoppingSerializer, pk)
 
-#@api_view(['GET', 'PUT', 'DELETE'])
-#def restaurant_by_pk(request, pk):
-#    """
-#    Resquests by restaurant pk for consult detail, edit or delete.
-#    """
-#
-#    try:
-#        restaurant = Restaurant.objects.get(pk=pk)
-#    except Restaurant.DoesNotExist:
-#        return HttpResponse(status=404)
-#
-#    if request.method == 'GET':
-#        serializer = RestaurantSerializer(restaurant)
-#        return JsonResponse(serializer.data)
-#
-#    elif request.method == 'PUT':
-#        data = JSONParser().parse(request)
-#        serializer = RestaurantSerializer(restaurant, data=data)
-#        if serializer.is_valid():
-#            serializer.save()
-#            return JsonResponse(serializer.data)
-#        return JsonResponse(serializer.errors, status=400)
-#
-#    elif request.method == 'DELETE':
-#        restaurant.delete()
-#        return HttpResponse(status=204)
-#
-@api_view(['GET', 'POST'])
-def post_or_get_shopping(request):
-    """
-    List all shoppings
-    """
+        elif request.method == 'PUT':
+            return TemplateClass.put(Shopping, ShoppingSerializer, pk)
+        
+        elif request.method == 'DELETE':
+            return TemplateClass.delete(Shopping, ShoppingSerializer, pk)
 
-    if request.method == 'GET':
-        shopping = Shopping.objects.all()
-        serializer = ShoppingSerializer(shopping, many = True)
-    
-    elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = ShoppingSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=201, safe=False)
-        return JsonResponse(serializer.errors, status=400, safe=False)
+class ItemView(TemplateClass):
+    @api_view(['GET', 'POST'])
+    def handle_get_or_post(request):
+        if request.method == 'GET':
+            return TemplateClass.get_all(Item, ItemSerializer)
+        if request.method == 'POST':
+            return TemplateClass.post(request, ItemSerializer)
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def shopping_by_pk(request, pk):
-    """
-    Resquests by restaurant pk for consult detail, edit or delete.
-    """
+    @api_view(['GET', 'PUT', 'DELETE'])
+    def handle_get_by_pk(request, pk):
+        if request.method == 'GET':
+            return TemplateClass.get_by_pk(Item, ItemSerializer, pk)
 
-    try:
-        shopping = Shopping.objects.get(pk=pk)
-    except Restaurant.DoesNotExist:
-        return HttpResponse(status=404)
+        elif request.method == 'PUT':
+            return TemplateClass.put(Item, ItemSerializer, pk)
+        
+        elif request.method == 'DELETE':
+            return TemplateClass.delete(Item, ItemSerializer, pk)
 
-    if request.method == 'GET':
-        serializer = ShoppingSerializer(shopping)
-    
-    elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = ShoppingSerializer(shopping, data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
-    
-    elif request.method == 'DELETE':
-        shopping.delete()
-        return HttpResponse(status=204)
+class MenuView(TemplateClass):
+    @api_view(['GET', 'POST'])
+    def handle_get_or_post(request):
+        if request.method == 'GET':
+            return TemplateClass.get_all(Menu, MenuSerializer)
+        if request.method == 'POST':
+            return TemplateClass.post(request, MenuSerializer)
 
-@api_view(['POST', 'GET'])
-def post_or_get_item(request):
-    """
-    List all item, or create a new item.
-    """
+    @api_view(['GET', 'PUT', 'DELETE'])
+    def handle_get_by_pk(request, pk):
+        if request.method == 'GET':
+            return TemplateClass.get_by_pk(Menu, MenuSerializer, pk)
 
-    if request.method == 'GET':
-        itens = Item.objects.all()
-        serializer = ItemSerializer(itens, many=True)
-        return JsonResponse(serializer.data, safe=False)
-
-    elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = ItemSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=201, safe=False)
-        return JsonResponse(serializer.errors, status=400, safe=False)
-
-@api_view(['GET', 'PUT', 'DELETE'])
-def item_by_pk(request, pk):
-    """
-    Resquests by item pk for consult detail, edit or delete.
-    """
-
-    try:
-        item = Item.objects.get(pk=pk)
-    except Item.DoesNotExist:
-        return HttpResponse(status=404)
-
-    if request.method == 'GET':
-        serializer = ItemSerializer(item)
-        return JsonResponse(serializer.data)
-
-    elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = ItemSerializer(item, data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
-
-    elif request.method == 'DELETE':
-        item.delete()
-        return HttpResponse(status=204)
-
-@api_view(['POST', 'GET'])
-def post_or_get_menu(request):
-    """
-    List all menu, or create a new menu.
-    """
-
-    if request.method == 'GET':
-        menus = Menu.objects.all()
-        serializer = MenuSerializer(menus, many=True)
-        return JsonResponse(serializer.data, safe=False)
-
-    elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = MenuSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=201, safe=False)
-        return JsonResponse(serializer.errors, status=400, safe=False)
-
-@api_view(['GET', 'PUT', 'DELETE'])
-def menu_by_pk(request, pk):
-    """
-    Resquests by menu pk for consult detail, edit or delete.
-    """
-
-    try:
-        menu = Menu.objects.get(pk=pk)
-    except Menu.DoesNotExist:
-        return HttpResponse(status=404)
-
-    if request.method == 'GET':
-        serializer = MenuSerializer(menu)
-        return JsonResponse(serializer.data)
-
-    elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = MenuSerializer(menu, data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
-
-    elif request.method == 'DELETE':
-        menu.delete()
-        return HttpResponse(status=204)
+        elif request.method == 'PUT':
+            return TemplateClass.put(Menu, MenuSerializer, pk)
+        
+        elif request.method == 'DELETE':
+            return TemplateClass.delete(Menu, MenuSerializer, pk)
 
 @api_view(['GET'])
 def see_qrcode(request, pk):
@@ -304,4 +178,3 @@ def see_qrcode(request, pk):
     }
 
     return JsonResponse(qrString , safe=False)
-
