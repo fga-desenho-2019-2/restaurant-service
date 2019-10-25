5eb3df5f0c40c7053656c3aebf23985cca52d44d
Sara Silva <saracsas2@gmail.com>
2019-10-25 01:33:39 -0300

    Update urls for news views

--- restaurant_service/api/urls.py
+++ restaurant_service/api/urls.py
@@ -2,23 +2,20 @@ from django.urls import path
 
 from .views import (
     RestaurantView,
-    post_or_get_shopping,
-    shopping_by_pk,
+    ShoppingView,
+    ItemView,
+    MenuView,
     see_qrcode,
-    post_or_get_item,
-    item_by_pk,
-    post_or_get_menu,
-    menu_by_pk,
 )
 
 urlpatterns = [
     path('restaurant/', RestaurantView.handle_get_or_post, name="restaurant"),
-    path('restaurant/<int:pk>/', RestaurantView.handle_get_by_pk, name="restaurant_by_pk"),
-    path('shopping/<int:pk>', shopping_by_pk, name="shopping_by_pk"),
-    path('shopping/', post_or_get_shopping, name="shopping"),
+    path('restaurant/<int:pk>', RestaurantView.handle_get_by_pk, name="restaurant_by_pk"),
+    path('shopping/', ShoppingView.handle_get_or_post, name="shopping"),
+    path('shopping/<int:pk>', ShoppingView.handle_get_by_pk, name="shopping_by_pk"),
+    path('item/', ItemView.handle_get_or_post, name="item"),
+    path('item/<int:pk>', ItemView.handle_get_by_pk, name="item_by_pk"),
+    path('menu/', MenuView.handle_get_or_post, name="menu"),
+    path('menu/<int:pk>', MenuView.handle_get_by_pk, name="menu_by_pk"),
     path('qrcode/<int:pk>', see_qrcode, name="see_qrcode"),
-    path('item/', post_or_get_item, name="item"),
-    path('item/<int:pk>/', item_by_pk, name="item_by_pk"),
-    path('menu/', post_or_get_menu, name="menu"),
-    path('menu/<int:pk>/', menu_by_pk, name="menu_by_pk"),
 ]
